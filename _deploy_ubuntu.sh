677b2c26cb445093bf7543280f1164e9c9e5e4d4
walterrosa2 <walterrosa2@hotmail.com>
2026-06-18 12:38:44 -0300

    fix: Remoção de 'sudo' no script de deploy e tag 'version' obsoleta do compose

--- _deploy_ubuntu.sh
+++ _deploy_ubuntu.sh
@@ -12,8 +12,5 @@ docker compose up -d --build
 # Removendo imagens órfãs
 docker image prune -f
 
-# Ajuste permissões para que o Docker acesse perfeitamente os volumes caso criado em root
-sudo chown -R 1000:1000 execucoes/ evid_provas.db || true
-
 echo "✅ Deploy concluído com sucesso!"
 docker compose ps
